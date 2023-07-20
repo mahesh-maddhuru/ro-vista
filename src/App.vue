d1c7112f0a4045e4cf25616d019e9deef8540f0d
Mahesh M <mahesh21319@gmail.com>
2023-07-20 11:56:23 -0400

    first commit with side bar

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import SideBar from "@/layouts/SideBar.vue";
+import AppHeader from "@/layouts/AppHeader.vue";
+</script>
+
+<template>
+  <v-app class="rounded rounded-md">
+    <app-header/>
+    <side-bar/>
+
+    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+      <RouterView />
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/layouts/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app-bar app clipped-left clipped-right color="blue-darken-4">
+    <v-toolbar-title class="align-left d-flex">
+      <span class="logo-icon">
+        <img src="@/assets/logo.png" width="190"/>
+      </span>
+
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+<script setup>
+</script>
