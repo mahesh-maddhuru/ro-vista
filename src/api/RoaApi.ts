0419afbcde67dd1c7237d5dc0e1926231a5e1ffb
Mahesh M <mahesh21319@gmail.com>
2023-07-23 01:04:39 -0400

    updated github workflows

--- src/api/RoaApi.ts
+++ src/api/RoaApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const staticIp = "localhost:8080";
+const staticIp = "ec2-3-15-251-42.us-east-2.compute.amazonaws.com";
 const base_url = `http://${staticIp}/rovista/api`;
 
 class RoaApi {
